57c9d109a2f45098c8776068499a42f16af36cd9
Denisa Fantová <denisafantova@gmail.com>
2022-10-06 20:59:51 +0200

    funkce: mizi pismena z prvniho slova

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
+const Wordbox = ({ word,  }) => {
   const [lettersLeft, setLettersLeft] = useState(word);  
+
+   const handleKeyUp = (e) => {
+    if (e.code ===  'Key' + lettersLeft[0].toUpperCase()) {
+      setLettersLeft((newWord) => newWord.substring(1))
+      console.log(lettersLeft.length)
+    }
+  };  
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => document.removeEventListener('keyup', handleKeyUp);
+  }, [lettersLeft]);  
   
   return (
     <div className="wordbox">{lettersLeft}</div>
